e705fdd91f25e8f1ab62360be1ce82778dcf57e0
Ben Greenman <benjaminlgreenman@gmail.com>
2017-08-20 21:01:56 -0400

    ** fix raco setup --check-pkg-deps

--- info.rkt
+++ info.rkt
@@ -1,9 +1,10 @@
 #lang info
 (define collection "gtp-paper")
-(define deps '("base" "pollen" "scribble-abbrevs"))
+(define deps '("base" "pollen" "scribble-abbrevs" "scribble-lib"))
 (define build-deps '("rackunit-lib" "racket-doc" "scribble-doc"))
 (define pkg-desc "Package for creating Scribble papers")
 (define version "0.0")
 (define pkg-authors '(ben))
 (define scribblings '(("docs/gtp-paper.scrbl" () (omit-start))))
 (define raco-commands '(("gtp-paper" (submod gtp-paper main) "Start a new paper" #f)))
+(define compile-omit-paths '("template"))
